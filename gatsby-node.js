700764f2864d86c597daa8b0ca968912de05f1a9
Marlo Boticano <m.boticano@gmail.com>
2019-08-29 11:19:15 -0700

    Commented out gastby-node.js & blogTemplate.js due to not having markdown pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,47 +1,47 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// /**
+//  * Implement Gatsby's Node APIs in this file.
+//  *
+//  * See: https://www.gatsbyjs.org/docs/node-apis/
+//  */
 
-// You can delete this file if you're not using it
+// // You can delete this file if you're not using it
 
-// Gatsby: Adding markdown pages
-const path = require(`path`)
+// // Gatsby: Adding markdown pages
+// const path = require(`path`)
 
-exports.createPages = async ({ actions, graphql, reporter }) => {
-  const { createPage } = actions
+// exports.createPages = async ({ actions, graphql, reporter }) => {
+//   const { createPage } = actions
 
-  const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
+//   const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
 
-  const result = await graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              path
-            }
-          }
-        }
-      }
-    }
-  `)
+//   const result = await graphql(`
+//     {
+//       allMarkdownRemark(
+//         sort: { order: DESC, fields: [frontmatter___date] }
+//         limit: 1000
+//       ) {
+//         edges {
+//           node {
+//             frontmatter {
+//               path
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  // Handle errors
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
+//   // Handle errors
+//   if (result.errors) {
+//     reporter.panicOnBuild(`Error while running GraphQL query.`)
+//     return
+//   }
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: node.frontmatter.path,
-      component: blogPostTemplate,
-      context: {}, // additional data can be passed via context
-    })
-  })
-}
+//   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//     createPage({
+//       path: node.frontmatter.path,
+//       component: blogPostTemplate,
+//       context: {}, // additional data can be passed via context
+//     })
+//   })
+// }
