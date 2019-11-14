bb8fdabd33d41871331d83a0051fb27b7d97218b
mdboticano <mdboticano@gmail.com>
2019-11-13 16:25:27 -0800

    Replaced files with default gatsby-blog-starter files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,47 +1,64 @@
-// /**
-//  * Implement Gatsby's Node APIs in this file.
-//  *
-//  * See: https://www.gatsbyjs.org/docs/node-apis/
-//  */
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// // You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
 
-// // Gatsby: Adding markdown pages
-// const path = require(`path`)
+  const blogPost = path.resolve(`./src/templates/blog-post.js`)
+  const result = await graphql(
+    `
+      {
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+              }
+            }
+          }
+        }
+      }
+    `
+  )
 
-// exports.createPages = async ({ actions, graphql, reporter }) => {
-//   const { createPage } = actions
+  if (result.errors) {
+    throw result.errors
+  }
 
-//   const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
+  // Create blog posts pages.
+  const posts = result.data.allMarkdownRemark.edges
 
-//   const result = await graphql(`
-//     {
-//       allMarkdownRemark(
-//         sort: { order: DESC, fields: [frontmatter___date] }
-//         limit: 1000
-//       ) {
-//         edges {
-//           node {
-//             frontmatter {
-//               path
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `)
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
 
-//   // Handle errors
-//   if (result.errors) {
-//     reporter.panicOnBuild(`Error while running GraphQL query.`)
-//     return
-//   }
+    createPage({
+      path: post.node.fields.slug,
+      component: blogPost,
+      context: {
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
+    })
+  })
+}
 
-//   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-//     createPage({
-//       path: node.frontmatter.path,
-//       component: blogPostTemplate,
-//       context: {}, // additional data can be passed via context
-//     })
-//   })
-// }
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
